2c7635365e45fbc5f85af10129cc09cf1d0c723c
DO Huy-Hoang <dohuyhoang88@gmail.com>
2013-12-04 00:42:10 +0100

    Implement test integration spark

--- savanna/tests/integration/tests/gating/test_spark_gating.py
+++ savanna/tests/integration/tests/gating/test_spark_gating.py
@@ -23,7 +23,7 @@ from savanna.tests.integration.tests import edp
 from savanna.tests.integration.tests import scaling
 
 
-class VanillaGatingTest(cluster_configs.ClusterConfigTest,
+class SparkGatingTest(cluster_configs.ClusterConfigTest,
                         scaling.ScalingTest, edp.EDPTest):
 
     SKIP_CLUSTER_CONFIG_TEST = \
@@ -46,7 +46,7 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
 
             node_group_template_slave_dn_id = self.create_node_group_template(
                 name='slave-dn',
-                plugin_config=self.vanilla_config,
+                plugin_config=self.spark_config,
                 description='test node group template',
                 volumes_per_node=0,
                 volume_size=0,
@@ -71,13 +71,12 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
 
             node_group_template_slave_id = self.create_node_group_template(
                 name='master',
-                plugin_config=self.vanilla_config,
+                plugin_config=self.spark_config,
                 description='test node group template',
                 volumes_per_node=0,
                 volume_size=0,
                 node_processes=['slave'],
                 node_configs={
-                    
                 }
             )
             node_group_template_id_list.append(node_group_template_slave_id)
@@ -99,7 +98,7 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
 
             node_group_template_dn_id = self.create_node_group_template(
                 name='dn',
-                plugin_config=self.vanilla_config,
+                plugin_config=self.spark_config,
                 description='test node group template',
                 volumes_per_node=0,
                 volume_size=0,
@@ -127,7 +126,7 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
 
             cluster_template_id = self.create_cluster_template(
                 name='test-cluster-template',
-                plugin_config=self.vanilla_config,
+                plugin_config=self.spark_config,
                 description='test cluster template',
                 cluster_configs={
                     'HDFS': cluster_configs.CLUSTER_HDFS_CONFIG,
@@ -146,9 +145,8 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
                     dict(
                         name='master-node-sec-nn',
                         flavor_id=self.flavor_id,
-                        node_processes=['secondarynamenode', 'oozie'],
+                        node_processes=['secondarynamenode'],
                         node_configs={
-                            #'JobFlow': cluster_configs.OOZIE_CONFIG
                         },
                         count=1),
                     dict(
@@ -160,7 +158,7 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
                         node_group_template_id=node_group_template_dn_id,
                         count=1),
                     dict(
-                        name='worker-node-tt',
+                        name='worker-node-slave',
                         node_group_template_id=node_group_template_slave_id,
                         count=1)
                 ]
@@ -182,7 +180,7 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
         try:
 
             cluster_info = self.create_cluster_and_get_info(
-                plugin_config=self.vanilla_config,
+                plugin_config=self.spark_config,
                 cluster_template_id=cluster_template_id,
                 description='test cluster',
                 cluster_configs={}
@@ -217,144 +215,6 @@ class VanillaGatingTest(cluster_configs.ClusterConfigTest,
                 message = 'Failure while cluster config testing: '
                 self.print_error_log(message, e)
 
-#----------------------------------EDP TESTING---------------------------------
-
-        job_data = open('integration/tests/resources/edp-job.pig').read()
-
-        lib_data = open('integration/tests/resources/edp-lib.jar').read()
-
-        job_jar_data = open('integration/tests/resources/edp-job.jar').read()
-
-        configs = {
-            "configs": {
-            "mapred.mapper.class": "org.apache.oozie.example.SampleMapper",
-            "mapred.reducer.class": "org.apache.oozie.example.SampleReducer"
-            }
-        }
-
-        try:
-
-            self._edp_testing('Pig', [{'pig': job_data}], [{'jar': lib_data}])
-
-        #TODO(vrovachev): remove mains after when bug #1237434 will be fixed
-            self._edp_testing('Jar', [{'pig': job_data}],
-                              [{'jar': job_jar_data}], configs)
-
-        except Exception as e:
-
-            with excutils.save_and_reraise_exception():
-
-                self.delete_objects(
-                    cluster_info['cluster_id'], cluster_template_id,
-                    node_group_template_id_list
-                )
-
-                message = 'Failure while EDP testing: '
-                self.print_error_log(message, e)
-
-#--------------------------------CLUSTER SCALING-------------------------------
-
-        change_list = [
-            {
-                'operation': 'resize',
-                'info': ['worker-node-tt-dn', 4]
-            },
-            {
-                'operation': 'resize',
-                'info': ['worker-node-dn', 0]
-            },
-            {
-                'operation': 'resize',
-                'info': ['worker-node-tt', 0]
-            },
-            {
-                'operation': 'add',
-                'info': [
-                    'new-worker-node-tt', 1, '%s' % node_group_template_slave_id
-                ]
-            },
-            {
-                'operation': 'add',
-                'info': [
-                    'new-worker-node-dn', 1, '%s' % node_group_template_dn_id
-                ]
-            }
-        ]
-
-        try:
-
-            new_cluster_info = self._cluster_scaling(cluster_info, change_list)
-
-        except Exception as e:
-
-            with excutils.save_and_reraise_exception():
-
-                self.delete_objects(
-                    cluster_info['cluster_id'], cluster_template_id,
-                    node_group_template_id_list
-                )
-
-                message = 'Failure while cluster scaling: '
-                self.print_error_log(message, e)
-
-        if not self.vanilla_config.SKIP_SCALING_TEST:
-
-#---------------------CLUSTER CONFIG TESTING AFTER SCALING---------------------
-
-            try:
-
-                self._cluster_config_testing(new_cluster_info)
-
-            except Exception as e:
-
-                with excutils.save_and_reraise_exception():
-
-                    self.delete_objects(
-                        new_cluster_info['cluster_id'], cluster_template_id,
-                        node_group_template_id_list
-                    )
-
-                    message = 'Failure while cluster config testing after ' \
-                              'cluster scaling: '
-                    self.print_error_log(message, e)
-
-#-----------------------MAP REDUCE TESTING AFTER SCALING-----------------------
-
-            try:
-
-                self._map_reduce_testing(new_cluster_info)
-
-            except Exception as e:
-
-                with excutils.save_and_reraise_exception():
-
-                    self.delete_objects(
-                        new_cluster_info['cluster_id'], cluster_template_id,
-                        node_group_template_id_list
-                    )
-
-                    message = 'Failure while Map Reduce testing after ' \
-                              'cluster scaling: '
-                    self.print_error_log(message, e)
-
-#--------------------CHECK SWIFT AVAILABILITY AFTER SCALING--------------------
-
-            try:
-
-                self._check_swift_availability(new_cluster_info)
-
-            except Exception as e:
-
-                with excutils.save_and_reraise_exception():
-
-                    self.delete_objects(
-                        new_cluster_info['cluster_id'], cluster_template_id,
-                        node_group_template_id_list
-                    )
-
-                    message = 'Failure during check of Swift availability ' \
-                              'after cluster scaling: '
-                    self.print_error_log(message, e)
 
 #----------------------------DELETE CREATED OBJECTS----------------------------
 
