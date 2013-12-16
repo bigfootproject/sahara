61c59afe4a3652df16758458b5929d8ee5cb0aac
VO_Thanh-Phuc <vo@eurecom.fr>
2013-12-16 01:28:12 +0100

    Fix init config bugs in config_helper.py (spark)

--- savanna/plugins/spark/config_helper.py
+++ savanna/plugins/spark/config_helper.py
@@ -37,12 +37,16 @@ XML_CONFS = {
 }
 
 SPARK_CONFS = {
-    "MASTER": {'SPARK_MASTER_IP', 'SPARK_MASTER_PORT',
-               'SPARK_MASTER_WEBUI_PORT', 'SPARK_WORKER_CORES',
-               'SPARK_WORKER_MEMORY', 'SPARK_WORKER_PORT',
-               'SPARK_WORKER_WEBUI_PORT', 'SPARK_WORKER_INSTANCES'
-               }
+    'SPARK': {
+             "BASE": ['SPARK_MASTER_IP', 'SLAVES'],
+             "OPTIONS": ['SPARK_MASTER_PORT',
+                    'SPARK_MASTER_WEBUI_PORT', 'SPARK_WORKER_CORES',
+                   'SPARK_WORKER_MEMORY', 'SPARK_WORKER_PORT',
+                   'SPARK_WORKER_WEBUI_PORT', 'SPARK_WORKER_INSTANCES'
+                   ]
+             }
 }
+
 
 # TODO(aignatov): Environmental configs could be more complex
 ENV_CONFS = {
@@ -120,8 +124,13 @@ def _initialise_configs():
                                     config_type="int"))
 
     for service, config_items in SPARK_CONFS.iteritems():
-        for name in config_items.iteritems():
-            configs.append(p.Config(name, service, "node"))
+        for name in config_items['BASE']:
+            cfg = p.Config(name, service, "cluster", priority=1)
+            configs.append(cfg)
+        for name in config_items['OPTIONS']:
+            cfg = p.Config(name, service, "cluster",
+                        is_optional=True, priority=2)
+            configs.append(cfg)
 
     configs.append(ENABLE_SWIFT)
     if CONF.enable_data_locality:
