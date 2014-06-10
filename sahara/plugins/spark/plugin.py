340c776a03ef2445eebbacd59879cf8e40c47428
Daniele Venzano <venza@brownhat.org>
2014-06-10 15:21:34 +0200

    Fix parameter order
    Change-Id: I757adaf8e6dc46ac144d965e09ba4e8f1a9437bb

--- sahara/plugins/spark/plugin.py
+++ sahara/plugins/spark/plugin.py
@@ -395,8 +395,8 @@ class SparkProvider(p.ProvisioningPluginBase):
                                  ' '.join(ng.node_processes))
 
         dn_amount = len(utils.get_instances(cluster, "datanode"))
-        rep_factor = c_helper.get_config_value(cluster, 'HDFS',
-                                               "dfs.replication")
+        rep_factor = c_helper.get_config_value('HDFS', "dfs.replication",
+                                               cluster)
 
         if dn_to_delete > 0 and dn_amount - dn_to_delete < rep_factor:
             raise ex.ClusterCannotBeScaled(
