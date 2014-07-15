dc5f8f7920313c0210a91748a6684e8c89d53ab8
Daniele Venzano <venza@brownhat.org>
2014-07-15 09:32:23 +0200

    Spark home is now a configuration option
    Change-Id: I2a0d35235f56d291ee24854ee38ee3f158933ffc

--- sahara/plugins/spark/scaling.py
+++ sahara/plugins/spark/scaling.py
@@ -35,8 +35,10 @@ def decommission_sl(master, inst_to_be_deleted, survived_inst):
     else:
         slaves_content = "\n"
 
+    cluster = master.node_group.cluster
+    sp_home = c_helper.get_config_value("Spark", "Spark home", cluster)
     r_master = remote.get_remote(master)
-    run.stop_spark(r_master)
+    run.stop_spark(r_master, sp_home)
 
     # write new slave file to master
     files = {'/opt/spark/conf/slaves': slaves_content}
@@ -47,7 +49,7 @@ def decommission_sl(master, inst_to_be_deleted, survived_inst):
         with remote.get_remote(i) as r:
             r.write_files_to(files)
 
-    run.start_spark_master(r_master)
+    run.start_spark_master(r_master, sp_home)
 
 
 def decommission_dn(nn, inst_to_be_deleted, survived_inst):
