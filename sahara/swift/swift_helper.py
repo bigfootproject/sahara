0a81d8b199804fd5af0847be08b4388dc7b2e0fc
Francesco Pace <pace.francesco.g@gmail.com>
2015-04-17 17:16:15 +0200

    Progress on Swift Credentials on Spark 1.2 and 1.3.

--- sahara/swift/swift_helper.py
+++ sahara/swift/swift_helper.py
@@ -71,6 +71,7 @@ def configure_master_for_swift_with_spark(master, wf, spark_home, configs):
 
     spark_defaults = os.path.join(wf, "conf/spark-defaults.conf")
     if swift_username is not None and swift_password is not None:
+        LOG.info("----> " + swift_password + " " + swift_username)
         r.execute_command("echo 'spark.hadoop." + HADOOP_SWIFT_USERNAME + "=" + swift_username + "' >> " +
                           spark_defaults)
         r.execute_command("echo 'spark.hadoop." + HADOOP_SWIFT_PASSWORD + "=" + swift_password + "' >> " +
